2e0a7d361c84f29bdbfb1738733a00ea5c907ad9
Phil <philippe.teigne@gmail.com>
2024-04-14 20:19:10 -0400

    adjust valdiation, add character length minimum, email uniqueness is case insensitive, password requires to match password_confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,14 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  PASSWORD_REQUIREMENTS = /\A
+  (?=.{8,}) # At least 8 characters long
+  /x
+
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :password, confirmation: true, format: PASSWORD_REQUIREMENTS
+  validates :password_confirmation, presence: true
+
 end
