909d2d777e0f9a09ad1054de24e951e79a2ddde6
Phil <philippe.teigne@gmail.com>
2024-04-15 20:34:39 -0400

    setup and install cypress

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-
     if user = User.authenticate_with_credentials(params[:email], params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
@@ -20,5 +19,4 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to '/login'
   end
-
 end
